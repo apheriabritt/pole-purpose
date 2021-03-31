cb9e8cc62c90eb520077dcaf56c472f293f2b091
britt wood <57709643+apheriabritt@users.noreply.github.com>
2021-03-31 10:28:32 +0100

    card ui with content fix

--- lib/CONSTANTS/card.dart
+++ lib/CONSTANTS/card.dart
@@ -2,6 +2,7 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:flip_card/flip_card.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:pole_purpose/CONSTANTS/appbar.dart';
 import 'package:pole_purpose/CONSTANTS/playSound.dart';
 
 ///i might re enter the card info into a FBDB.
@@ -10,34 +11,75 @@ Widget SingleCard(String id,title,content){
   return StatefulBuilder(
       builder: (context, setState) {
         return Container(
+          //height:MediaQuery.of(context).size.height,
+          //width:MediaQuery.of(context).size.width,
           child: Padding(
             padding: const EdgeInsets.all(0.0),
-            child: FlipCard(
-              direction: FlipDirection.HORIZONTAL,
-              front: FittedBox(
-                child: Container(
-                  child: Card(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(25.0),
-                    ),
-                    child: Center(child: Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(title,style:TextStyle(fontFamily: 'Xtreem',fontSize:50)),
+            child: GestureDetector(
+              onLongPress: (){
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => Scaffold(
+                          appBar: customAppBarjustback,
+                          extendBodyBehindAppBar: true,
+                          body: Padding(
+                            padding: const EdgeInsets.all(25.0),
+                            child: Center(child: SingleCard( id,title,content)),
+                          )
+                        )));
+              },
+              child: FlipCard(
+                direction: FlipDirection.HORIZONTAL,
+                front: FittedBox(
+                  child: Container(
+                    height:1000,width:650,
+                    child: Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(25.0),
                       ),
-                      Padding(
-                        padding: const EdgeInsets.all(5.0),
-                        child:
-                        //Image.network(image,fit:BoxFit.contain,width:MediaQuery.of(context).size.width/1)
-                        Image.asset('images/assets/CARDS/$id.png',fit:BoxFit.contain,width:MediaQuery.of(context).size.width/1),
-                      ),
-                    ],
-                  )), elevation: 10,),
+                      child: Center(child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(title,style:TextStyle(fontFamily: 'Xtreem',fontSize:75)),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(5.0),
+                          child:
+                          //Image.network(image,fit:BoxFit.contain,width:MediaQuery.of(context).size.width/1)
+                          Image.asset('images/assets/CARDS/$id.png',fit:BoxFit.fill),
+                        ),
+                      ],
+                    )), elevation: 10,),
+                  ),
                 ),
-              ),
-              back: FittedBox(
-                ///do this at the end
+                back: FittedBox(
+                  child: Container(
+                    height:1000,width:650,
+                    child: Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(25.0),
+                      ),
+                      child: Center(child: Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(title,style:TextStyle(fontFamily: 'Xtreem',fontSize:75)),
+                          ),
+                          Container(
+                            height:800,
+                            child: Padding(
+                              padding: const EdgeInsets.all(25.0),
+                              child:
+                                Center(child: SingleChildScrollView(child: Text(content,style:TextStyle(fontSize: 35))))
+                              //Image.network(image,fit:BoxFit.contain,width:MediaQuery.of(context).size.width/1)
+                            ),
+                          ),
+                        ],
+                      )), elevation: 10,),
+                  ),
+                ),
               ),
             ),
           ),
