e961fed52672746d5e9a8f314c01b3dd05da4c62
britt wood <57709643+apheriabritt@users.noreply.github.com>
2021-05-06 08:40:13 +0100

    android and ios new version 3.1.1 with fixed blog and push notifications

--- lib/BLOG/blog.dart
+++ lib/BLOG/blog.dart
@@ -2,9 +2,21 @@ import 'dart:convert';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_html/flutter_html.dart';
 import 'package:http/http.dart' as http;
 import 'package:pole_purpose/CONSTANTS/appbar.dart';
+import 'package:pole_purpose/CONSTANTS/loading.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
+class BlogPost
+
+{
+
+  String title,date,url;
+
+  BlogPost(this.title,this.date,this.url);
+
+}
 
 class Blog extends StatefulWidget {
   @override
@@ -12,23 +24,35 @@ class Blog extends StatefulWidget {
 }
 
 
-_getPosts() async {
-  final response = await http.get(Uri.parse('https://www.polepurpose.com/index.php/wp-json/wp/v2/posts'));
-  print('initial response: ${response.body}');
-  List decoded = json.decode(response.body);
-  print('first in list');
-  print(decoded.first);
-  Set decodemap = decoded.toSet();
-  print('map:');
-  decodemap.forEach((element) {
-   print('title:');print( element['title']);
-
-  });
-
-}
-
 
 class _BlogState extends State<Blog> {
+  Set decodemap;
+  bool loading=true;
+  List<BlogPost> blogList = [];
+
+  void _getPosts() async {
+    final response = await http.get(Uri.parse('https://www.polepurpose.com/index.php/wp-json/wp/v2/posts'));
+    List decoded = json.decode(response.body);
+    decodemap = decoded.toSet();
+    decodemap.forEach((element) {
+
+      print('title:${element['title']['rendered'].toString()}');
+      print('date:${element['date'].toString()}');
+      print('link:${element['link'].toString()}');
+
+      BlogPost post = new BlogPost(
+        element['title']['rendered'],
+        element['date'],
+        element['link'],
+      );
+      blogList.add(post);
+      print('done');
+
+    });
+    setState(() {
+      loading=false;
+    });
+  }
   @override
   void initState() {
     super.initState();
@@ -37,19 +61,69 @@ class _BlogState extends State<Blog> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    print('building');
+    return loading==true?Loading():Scaffold(
               appBar:customAppBarjustback,
-              body: ListView(
-                children: <Widget>[
-                  Container(
-                    height: MediaQuery.of(context).size.height * .35,
-                    margin: const EdgeInsets.symmetric(vertical: 15.0),
-                    child: Text(
-                      'soon'
+              body: ListView.builder(
+                itemCount: blogList.length,
+                itemBuilder: (context, index) {
+                  return GestureDetector(
+                    onTap: () {
+                      Navigator.of(context).push(PageRouteBuilder(
+                          opaque: false,
+                          pageBuilder: (BuildContext context, _, __) {
+                            return
+                              SafeArea(
+                                child: Scaffold(extendBodyBehindAppBar: true,
+                                    backgroundColor: Colors.white,
+                                    floatingActionButtonLocation: FloatingActionButtonLocation
+                                        .startTop,
+                                    floatingActionButton:
+                                    Padding(
+                                      padding: const EdgeInsets.fromLTRB(
+                                          0, 15, 0, 0),
+                                      child: FloatingActionButton(
+                                        backgroundColor: Colors.black,
+                                        child: Icon(Icons.arrow_back),
+                                        onPressed: () {
+                                          Navigator.pop(context);
+                                        },
+                                      ),
+                                    ),
+                                    body: Material(
+                                        child: Padding(
+                                          padding: const EdgeInsets.all(8.0),
+                                          child: WebView(
+                                            initialUrl:blogList[index].url ,
+                                            javascriptMode: JavascriptMode
+                                                .unrestricted,
+                                          ),
+                                        ))
+                                ),
+                              );
+                            ////////////////////
+                          }));
+                    }, //push to new page
+                    child: Card(
+                      shape: RoundedRectangleBorder(
+                          side: BorderSide(color: Colors.black, width: 2),
+                          borderRadius: BorderRadius.circular(20)),
+                      color: Colors.white,
+                      shadowColor: Colors.black,
+                      elevation: 5.0,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: ListTile(
+                            title:  Html(data:blogList[index].title),
+                            subtitle: Text(blogList[index].date.replaceAll('T', ' ')),
+                            trailing: Icon(Icons.arrow_forward_ios)
+
+                        ),
                       ),
-                  ),
-                ],
-              ),
+                    ),
+                  );
+                }
+              )
             );
   }
 }
