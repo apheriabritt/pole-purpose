1b52c92c154eb1ea89f11df24a6cfa07f7fac2ac
britt wood <57709643+apheriabritt@users.noreply.github.com>
2021-06-10 14:11:14 +0100

    version code 3 for android release

--- android/app/src/main/kotlin/com/example/pole/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/pole/MainActivity.kt
@@ -4,17 +4,14 @@ import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
-import android.view.WindowManager; 
+import android.view.WindowManager;
 import android.view.WindowManager.LayoutParams;
 import android.os.Bundle;
 
 class MainActivity: FlutterActivity() {
-    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-        GeneratedPluginRegistrant.registerWith(flutterEngine);
-    }
-    
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState);
-        getWindow().addFlags(LayoutParams.FLAG_SECURE);
-    }
+  override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+    GeneratedPluginRegistrant.registerWith(flutterEngine);
+  }
+
+
 }
